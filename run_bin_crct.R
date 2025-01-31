d8bb76767491363d733626708d3218a6c316a86a
Erin Nolan <143678713+ErnKNolan@users.noreply.github.com>
2025-01-31 16:23:01 +1100

    Update run_bin_crct.R
    Updating to project 3 code

--- run_bin_crct.R
+++ run_bin_crct.R
@@ -1,89 +1,64 @@
 #AUTHOR: Erin Nolan
-#TITLE: PROJECT 2 NON-ADAPTIVE SIMULATION
+#TITLE: NON-ADAPTIVE SIMULATIONS FOR OPTIMISING IMPLEMENTATION STRATEGIES
 #PURPOSE: RUNS THE SIMULATIONS FOR MULTIARM cRCT UNDER VARIOUS PROPERTIES
 
 #Run the functions defined in make_clusters and fit_bae
 pacman::p_load(here,future.apply,tictoc,car,ggforce,rsimsum,dplyr,cmdstanr,rstan)
 
-source(here("Programs","make_clusters.R"))
-source(here("Programs","fit_bae.R"))
+source("make_clusters.R")
+source("runNonAdaptTrial.R")
+source("testInterim.R")
 
-#PROJECT 2 
 #The different trial properties
-set.seed(580208819)
-properties <- expand.grid(trt_eff_scen = c(1,2,3), ctrl_prop = c(0.1,0.35), icc = c(0.05,0.2), n_per_k = c(5,75), k = c(5,20))
-properties <- properties %>%
-  mutate(k = ifelse(n_per_k == 75 & k == 20,10,k)) #instead of 20 clusters with 75 per, its 10 clusters
-
-#4OCT Add in n=5 k=10
-oct_prop <- expand.grid(trt_eff_scen = c(1,2,3), ctrl_prop = c(0.1,0.35), icc = c(0.05,0.2), n_per_k = 5, k = 10)
-
-#5OCT Add in n=50 and n=100 for ctrl prop = 0.1
-oct2_prop <- expand.grid(trt_eff_scen = c(1,2,3), ctrl_prop = c(0.1), icc = c(0.05,0.2), n_per_k = c(50,100), k = 10)
-
-#6OCT Add in n=50 and n=100 for ctrl prop = 0.1 and k=5
-oct3_prop <- expand.grid(trt_eff_scen = c(1,2,3), ctrl_prop = c(0.1), icc = c(0.05,0.2), n_per_k = c(50,100), k = 5)
-
-#9OCT Add in n=25 for k=5 and k=10
-oct4_prop <- expand.grid(trt_eff_scen = c(1,2,3), ctrl_prop = c(0.1), icc = c(0.05,0.2), n_per_k = c(25), k = c(5,10))
+set.seed(656038738)
+#trt_eff_scen = the treatment scenario
+#ctrl_prop = the baseline proportion of the event of interest
+#icc = intra-class correlation
+#n_per_k = number of participants per cluster
+#k = number of clusters
+properties <- expand.grid(trt_eff_scen = c(2,3), ctrl_prop = c(0.1), icc = c(0.05,0.2), n_per_k = c(25,50,75), k = c(15,25),nblock=1)
 
 #bind to properties
-properties <- rbind(properties,oct_prop,oct2_prop,oct3_prop,oct4_prop) %>%
-  mutate(t1 = case_when(trt_eff_scen == 1 ~ ctrl_prop+0.5,
-                        trt_eff_scen == 2 ~ ctrl_prop+0.4,
+properties <- rbind(properties) %>%
+  mutate(t4 = case_when(trt_eff_scen == 2 ~ ctrl_prop+0.4,
                         trt_eff_scen == 3 ~ ctrl_prop+0),
-         t2 = case_when(trt_eff_scen == 1 ~ ctrl_prop+0.3,
-                        trt_eff_scen == 2 ~ ctrl_prop+0.3,
+         t3 = case_when(trt_eff_scen == 2 ~ ctrl_prop+0.3,
                         trt_eff_scen == 3 ~ ctrl_prop+0),
-         t3 = case_when(trt_eff_scen == 1 ~ ctrl_prop+0.1,
-                        trt_eff_scen == 2 ~ ctrl_prop+0.2,
+         t2 = case_when(trt_eff_scen == 2 ~ ctrl_prop+0.2,
                         trt_eff_scen == 3 ~ ctrl_prop+0),
-         t4 = ctrl_prop)
+         t1 = ctrl_prop,
+         draws = ifelse(n_per_k == 75,1250,750))
 
-#loop the data generation
-outdat <- list()
-for(j in 1:nrow(properties)){
-  outdat[[j]] <- makeClusters(t=4,nid=properties$n_per_k[j],t1=properties$k[j],t2=properties$k[j],t3=properties$k[j],t4=properties$k[j])
-}
-j <- 1
+drawsdat <- properties$draws
+properties <- properties %>% dplyr::select(-draws)
+
+
 #set how many workers you want to use
 plan(multisession,workers=20) 
-rstan_options(auto_write = TRUE)
-#Work comp (comment out the irrelevant one)
-#Sys.setenv(Home="C:/Users/Enolan")
-#baepath <- "C:/Users/ENolan/Simulations/runbae.stan"
-#set_cmdstan_path("C:/Users/ENolan/.cmdstan/cmdstan-2.33.1")
-#outdir <- 
-#Home comp (comment out the irrelevant one)
-#Sys.setenv(Home="D:/Programs")
-baepath <- "D:/Programs/PhDProject2/Programs/runbae.stan"
-set_cmdstan_path(path="C:/Users/nolan/Documents/.cmdstan/cmdstan-2.33.1")
-#outdir <- "D:/Programs/Simulations"
 
-#run the code
-#test <- list()
-tic()
+#Put in the paths for the simulation
+baepath <- "adapt_arm2.stan" #the file path for the Stan model code
+set_cmdstan_path(path="/root/.cmdstan/cmdstan-2.33.1") #where cmdstan is located
+outdir <- "SimTrash" #where the stan files will be output
 mod <- cmdstan_model(baepath, pedantic = F, compile=T)
-#test <- readRDS(here("simdata2.RDS"))
-#THIS IS LEFT ON WHAT I RAN LAST
-for(j in 85:96){
-  test[[length(test)+1]] <- future_replicate(2500,testss(expdat=outdat[[j]],t=4,mod=mod,
-                                           rho=properties$icc[j],t1=properties$t1[j],t2=properties$t2[j],t3=properties$t3[j],t4=properties$t4[j]),
-                                 future.seed = 42L)
+
+#run the trial
+#test <- list() #test is the list of all the output from the simulated trials
+for(j in 1:24){ #loops through properties 1 to 24
+  test[[length(test)+1]] <- future_replicate(5,runNonAdaptTrial(properties,mod,outdir,j,t=t,draws=drawsdat[j]),
+                                             future.seed = 42L)
+  saveRDS(test,"nonadapt_test.RDS")
 }
-toc(quiet=FALSE)
-#saveRDS(test,here("simdata.RDS"))
-#Reframe the output for use
-tempd <- test
-for(j in c(1:96)){
-  for(i in 1:2500){
-    tempd[[j]][[i]]$sim <- i
-    tempd[[j]][[i]]$property <- j
+
+nonadapt <- list()
+for(j in 1:24){
+  for(i in seq(1,12500,5)){
+    nonadapt[[length(nonadapt)+1]] <- test[[j]][[i]]
+    nonadapt[[length(nonadapt)]]$sim <- (i+4)/5
+    nonadapt[[length(nonadapt)]]$property <- j
   }
 }
-
-outsim <- bind_rows(tempd)
-
+nonadapt <- bind_rows(nonadapt)
 properties2 <- properties %>% mutate(row = row_number()) 
-outsim2 <- merge(outsim,properties2,by.y=c("row"),by.x="property")
-#saveRDS(outsim2,file=here("testsim.RDS"))
+nonadapt_out <- merge(nonadapt,properties2,by.y=c("row"),by.x="property")
+saveRDS(nonadapt_out,here("Data","nonadapt_out.RDS"))
