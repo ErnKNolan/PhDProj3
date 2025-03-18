b5d543449535b7096eea6685188f6c6c704a3a54
Erin Nolan <143678713+ErnKNolan@users.noreply.github.com>
2025-03-18 11:47:15 +1100

    Update arm_drop_differences.R

--- arm_drop_differences.R
+++ arm_drop_differences.R
@@ -111,3 +111,94 @@ narm_nint_arm4 <- narmdrop %>% filter(Scenario=="Effect", Interim == `Number of 
               values_from = `Arm 4`,
               names_from = `Number of interims`) %>%
   mutate(diff2_1 = `2` - `1`)
+
+#Null scenario
+#n interims
+#Arm 2
+narm_nullnint_arm2 <- narmdrop %>% filter(Scenario=="Null", Interim == `Number of interims`) %>%
+  pivot_wider(id_cols=c(n_per_k,`N clusters per arm`,ICC),
+              values_from = `Arm 2`,
+              names_from = `Number of interims`) %>%
+  mutate(diff2_1 = `2` - `1`)
+
+#Arm 3
+narm_nullnint_arm3 <- narmdrop %>% filter(Scenario=="Null", Interim == `Number of interims`) %>%
+  pivot_wider(id_cols=c(n_per_k,`N clusters per arm`,ICC),
+              values_from = `Arm 3`,
+              names_from = `Number of interims`) %>%
+  mutate(diff2_1 = `2` - `1`)
+
+#Arm 4
+narm_nullnint_arm4 <- narmdrop %>% filter(Scenario=="Null", Interim == `Number of interims`) %>%
+  pivot_wider(id_cols=c(n_per_k,`N clusters per arm`,ICC),
+              values_from = `Arm 4`,
+              names_from = `Number of interims`) %>%
+  mutate(diff2_1 = `2` - `1`)
+
+#ICC
+#Arm 2
+narm_nullicc_arm2 <- narmdrop %>% filter(Scenario=="Null") %>%
+  pivot_wider(id_cols=c(n_per_k,`N clusters per arm`,`Number of interims`,Interim),
+              values_from = `Arm 2`,
+              names_from = ICC) %>%
+  mutate(diff02_005 = `0.2` - `0.05`)
+
+#Arm 3
+narm_nullicc_arm3 <- narmdrop %>% filter(Scenario=="Null") %>%
+  pivot_wider(id_cols=c(n_per_k,`N clusters per arm`,`Number of interims`,Interim),
+              values_from = `Arm 3`,
+              names_from = ICC) %>%
+  mutate(diff02_005 = `0.2` - `0.05`)
+
+#Arm 4
+narm_nullicc_arm4 <- narmdrop %>% filter(Scenario=="Null") %>%
+  pivot_wider(id_cols=c(n_per_k,`N clusters per arm`,`Number of interims`,Interim),
+              values_from = `Arm 4`,
+              names_from = ICC) %>%
+  mutate(diff02_005 = `0.2` - `0.05`)
+
+
+#Stops
+#Effect scenario
+#ICC
+stops_icc <- stops %>% filter(Scenario=="Effect",`N interims` == 2) %>%
+  pivot_wider(id_cols=c(n_per_k,`N clusters per arm`),
+              values_from = `Stopped interim 2`,
+              names_from = ICC) %>%
+  mutate(diff02_005 = `0.2` - `0.05`)
+
+#n per k
+stops_nperk <- stops %>% filter(Scenario=="Effect",`N interims` == 2) %>%
+  pivot_wider(id_cols=c(ICC,`N clusters per arm`),
+              values_from = `Stopped interim 2`,
+              names_from = n_per_k) %>%
+  mutate(diff75_50 = `75` - `50`,diff50_25 = `50` - `25`)
+
+#n clusters
+stops_k <- stops %>% filter(Scenario=="Effect",`N interims` == 2) %>%
+  pivot_wider(id_cols=c(n_per_k,ICC),
+              values_from = `Stopped interim 2`,
+              names_from = `N clusters per arm`) %>%
+  mutate(diff25_15 = `25` - `15`)
+
+#Null scenario
+#ICC
+stops_null_icc <- stops %>% filter(Scenario=="Null") %>%
+  pivot_wider(id_cols=c(n_per_k,`N interims`,`N clusters per arm`),
+              values_from = `Stopped total`,
+              names_from = ICC) %>%
+  mutate(diff02_005 = `0.2` - `0.05`)
+
+#n per k
+stops_null_nperk <- stops %>% filter(Scenario=="Null") %>%
+  pivot_wider(id_cols=c(ICC,`N clusters per arm`,`N interims`),
+              values_from = `Stopped total`,
+              names_from = n_per_k) %>%
+  mutate(diff75_50 = `75` - `50`,diff50_25 = `50` - `25`)
+
+#n clusters
+stops_null_k <- stops %>% filter(Scenario=="Null") %>%
+  pivot_wider(id_cols=c(n_per_k,ICC,`N interims`),
+              values_from = `Stopped total`,
+              names_from = `N clusters per arm`) %>%
+  mutate(diff25_15 = `25` - `15`)
