57c71354e47e39afc84ae782cf43a42efa0e8f72
Erin <111838888+ErnNolan@users.noreply.github.com>
2025-06-30 18:33:17 +1000

    Update make_clusters.R

--- make_clusters.R
+++ make_clusters.R
@@ -1,5 +1,5 @@
 #AUTHOR: Erin Nolan
-#TITLE: PROJECT 3 FUNCTION MAKE CLUSTERS
+#TITLE: PROJECT 2 FUNCTION MAKE CLUSTERS
 #PURPOSE: FUNCTION TO MAKE THE GENERATED DATASET
 
 pacman::p_load(tidyverse,purrr,lme4,MASS,lsmeans)
@@ -12,14 +12,11 @@ pacman::p_load(tidyverse,purrr,lme4,MASS,lsmeans)
 makeClusters <- function(t,nid,...){
   int <- list()
   clusters <- list(...) #the number of clusters in each group
-  nclusters <- cumsum(clusters) #cumulative n for the for loop
+  #nclusters <- cumsum(clusters) #cumulative n for the for loop
   for(i in 1:t){
-    if (i==1){ #first group has cluster starting at 1
-      int[[i]] <- expand.grid(iid = c(1:nid), site = seq(1,nclusters[i],1), trt = i)
-    }
-    else {
-      int[[i]] <- expand.grid(iid = c(1:nid), site = seq(nclusters[i-1]+1,nclusters[i],1), trt = i)
-    }
+   # if (i==1){ #first group has cluster starting at 1
+      int[[i]] <- expand.grid(iid = c(1:nid), site = seq(1,clusters[[i]],1), trt = i)
+   # }
   }
   groups <- map_df(int, ~as.data.frame(.)) #unlist them and make them a dataframe
   return(groups)
