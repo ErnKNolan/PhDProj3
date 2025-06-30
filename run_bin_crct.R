38959e5b7176c0dc985d67f476d2cd62e46790b4
Erin <111838888+ErnNolan@users.noreply.github.com>
2025-06-30 18:47:25 +1000

    Update run_bin_crct.R

--- run_bin_crct.R
+++ run_bin_crct.R
@@ -2,10 +2,11 @@
 #TITLE: NON-ADAPTIVE SIMULATIONS FOR OPTIMISING IMPLEMENTATION STRATEGIES
 #PURPOSE: RUNS THE SIMULATIONS FOR MULTIARM cRCT UNDER VARIOUS PROPERTIES
 
-#Run the functions defined in make_clusters and nonAdaptTrial
+#Run the functions defined in make_clusters and fit_bae
 pacman::p_load(here,future.apply,tictoc,car,ggforce,rsimsum,dplyr,cmdstanr,rstan)
 
 source("make_clusters.R")
+source("testInterim.R")
 source("nonAdaptTrial.R")
 
 #The different trial properties
@@ -15,16 +16,25 @@ set.seed(656038738)
 #icc = intra-class correlation
 #n_per_k = number of participants per cluster
 #k = number of clusters
-properties <- expand.grid(trt_eff_scen = c(2,3), ctrl_prop = c(0.1), icc = c(0.05,0.2), n_per_k = c(25,50,75), k = c(15,25),nblock=1)
+properties <- expand.grid(trt_eff_scen = c(5), ctrl_prop = c(0.1), icc = c(0.05,0.2), n_per_k = c(25,50,75), k = c(15,25),nblock=1)
 
 #bind to properties
 properties <- rbind(properties) %>%
-  mutate(t4 = case_when(trt_eff_scen == 2 ~ ctrl_prop+0.4,
-                        trt_eff_scen == 3 ~ ctrl_prop+0),
-         t3 = case_when(trt_eff_scen == 2 ~ ctrl_prop+0.3,
-                        trt_eff_scen == 3 ~ ctrl_prop+0),
-         t2 = case_when(trt_eff_scen == 2 ~ ctrl_prop+0.2,
-                        trt_eff_scen == 3 ~ ctrl_prop+0),
+  mutate(t4 = case_when(trt_eff_scen == 1 ~ ctrl_prop+0.5,
+                        trt_eff_scen == 2 ~ ctrl_prop+0.4,
+                        trt_eff_scen == 3 ~ ctrl_prop+0,
+                        trt_eff_scen == 4 ~ ctrl_prop+0.4,
+                        trt_eff_scen == 5 ~ ctrl_prop+0.4),
+         t3 = case_when(trt_eff_scen == 1 ~ ctrl_prop+0.3,
+                        trt_eff_scen == 2 ~ ctrl_prop+0.3,
+                        trt_eff_scen == 3 ~ ctrl_prop+0,
+                        trt_eff_scen == 4 ~ ctrl_prop+0.1,
+                        trt_eff_scen == 5 ~ ctrl_prop+0.35),
+         t2 = case_when(trt_eff_scen == 1 ~ ctrl_prop+0.1,
+                        trt_eff_scen == 2 ~ ctrl_prop+0.2,
+                        trt_eff_scen == 3 ~ ctrl_prop+0,
+                        trt_eff_scen == 4 ~ ctrl_prop+0,
+                        trt_eff_scen == 5 ~ ctrl_prop+0.25),
          t1 = ctrl_prop,
          draws = ifelse(n_per_k == 75,1250,750))
 
@@ -42,15 +52,15 @@ mod <- cmdstan_model(baepath, pedantic = F, compile=T)
 
 #run the trial
 #test <- list() #test is the list of all the output from the simulated trials
-for(j in 1:24){ #loops through properties 1 to 24
+for(j in 1:12){ #loops through properties 1 to 24
   test[[length(test)+1]] <- future_replicate(2500,nonAdaptTrial(properties,mod,outdir,j,t=t,draws=drawsdat[j]),
                                              future.seed = 42L)
-  saveRDS(test,"nonadapt_jan25.RDS")
+  saveRDS(test,"nonadapt_newscen.RDS")
 }
 
 nonadapt <- list()
-for(j in 1:24){
-  for(i in 1:10){
+for(j in 1){
+  for(i in 1:2500){
     nonadapt[[length(nonadapt)+1]] <- test[[j]][[i]]
     nonadapt[[length(nonadapt)]]$sim <- i
     nonadapt[[length(nonadapt)]]$property <- j
